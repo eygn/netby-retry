df45cee7b0e7e28df0f8a12fb996308afcdc0064
byg <yuangang.byg@alibaba-inc.com>
2023-07-08 11:53:04 +0800

    rm dependencies

--- netby-biz-retry/src/main/java/com/netby/biz/retry/entity/BizRetryDO.java
+++ netby-biz-retry/src/main/java/com/netby/biz/retry/entity/BizRetryDO.java
@@ -3,6 +3,7 @@ package com.netby.biz.retry.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -14,8 +15,10 @@ import java.util.Date;
  * @since 2023-06-17 17:09:48
  */
 @SuppressWarnings("serial")
+@Data
 @TableName("biz_retry")
 public class BizRetryDO extends Model<BizRetryDO> {
+
     //主键
     private Integer id;
     //业务编号
@@ -38,95 +41,6 @@ public class BizRetryDO extends Model<BizRetryDO> {
     private Date modified;
     //数据是否有效
     private Integer flag;
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getBizNo() {
-        return bizNo;
-    }
-
-    public void setBizNo(String bizNo) {
-        this.bizNo = bizNo;
-    }
-
-    public String getBizType() {
-        return bizType;
-    }
-
-    public void setBizType(String bizType) {
-        this.bizType = bizType;
-    }
-
-    public String getBizParams() {
-        return bizParams;
-    }
-
-    public void setBizParams(String bizParams) {
-        this.bizParams = bizParams;
-    }
-
-    public String getRetryType() {
-        return retryType;
-    }
-
-    public void setRetryType(String retryType) {
-        this.retryType = retryType;
-    }
-
-    public Integer getRetryCount() {
-        return retryCount;
-    }
-
-    public void setRetryCount(Integer retryCount) {
-        this.retryCount = retryCount;
-    }
-
-    public Integer getRetryStatus() {
-        return retryStatus;
-    }
-
-    public void setRetryStatus(Integer retryStatus) {
-        this.retryStatus = retryStatus;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public Date getModified() {
-        return modified;
-    }
-
-    public void setModified(Date modified) {
-        this.modified = modified;
-    }
-
-    public Integer getFlag() {
-        return flag;
-    }
-
-    public void setFlag(Integer flag) {
-        this.flag = flag;
-    }
 
     /**
      * 获取主键值
--- netby-biz-retry/src/main/java/com/netby/biz/retry/service/BizRetryService.java
+++ netby-biz-retry/src/main/java/com/netby/biz/retry/service/BizRetryService.java
@@ -2,6 +2,7 @@ package com.netby.biz.retry.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.netby.biz.retry.entity.BizRetryDO;
+import com.netby.biz.retry.enums.RetryStatusEnum;
 
 /**
  * 服务重试表(BizRetry)表服务接口
@@ -10,6 +11,14 @@ import com.netby.biz.retry.entity.BizRetryDO;
  * @since 2023-06-17 17:09:50
  */
 public interface BizRetryService extends IService<BizRetryDO> {
+
+    /**
+     * 更新重试
+     *
+     * @param riskDecisionRetryDO
+     * @param retryStatusEnum
+     */
+    void updateRetry(BizRetryDO riskDecisionRetryDO, RetryStatusEnum retryStatusEnum);
 
 }
 
